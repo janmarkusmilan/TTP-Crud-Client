a04302d45993229726de082b220eb60fd03ddeb1
Jan Markus Milan <jmarkusm@me.com>
2020-06-12 10:55:42 -0400

    Preparing for Edit Campus

---
+++ src/components/containers/EditCampusFormContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class EditCampusFormContainer extends Component {
+  render() {
+    return <h1>Hello from EditCampusFormContainer</h1>;
+  }
+}
+
+export default EditCampusFormContainer;
